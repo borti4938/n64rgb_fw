eaf5ffff2c5d6352bc0b2b17787beecef1a8a346
borti4938 <borti4938@gmail.com>
2022-03-27 16:06:04 +0200

    simplify everything a bit - remove extra register for vi-deblur and 16bit mode configuration - move data counter to demux - simplify interlace detection - add internal reset

--- rtl/n64_vdemux.v
+++ rtl/n64_vdemux.v
@@ -34,88 +34,99 @@
 module n64_vdemux(
   VCLK,
   nDSYNC,
+  nRST,
 
   D_i,
   demuxparams_i,
 
-  vdata_r_0,
-  vdata_r_1
+  vdata_sy_0_o,
+  vdata_1_o
 );
 
-`include "../vh/n64rgb_params.vh"
+`include "../lib/n64rgb_params.vh"
 
 input VCLK;
 input nDSYNC;
+input nRST;
 
 input  [color_width-1:0] D_i;
-input  [            4:0] demuxparams_i;
+input  [            2:0] demuxparams_i;
 
-output reg [`VDATA_FU_SLICE] vdata_r_0; // buffer for sync, red, green and blue
-output reg [`VDATA_FU_SLICE] vdata_r_1; // (unpacked array types in ports requires system verilog)
+output [`VDATA_SY_SLICE] vdata_sy_0_o; // buffer for sync, red, green and blue
+output [`VDATA_FU_SLICE] vdata_1_o; // (unpacked array types in ports requires system verilog)
 
 
 // unpack demux params
 
-wire [1:0] data_cnt    = demuxparams_i[4:3];
-wire       vmode       = demuxparams_i[  2];
-wire       ndo_deblur  = demuxparams_i[  1];
-wire       n16bit_mode = demuxparams_i[  0];
+wire palmode     = demuxparams_i[  2];
+wire ndo_deblur  = demuxparams_i[  1];
+wire n16bit_mode = demuxparams_i[  0];
 
 
 // start of rtl
 
-reg fetch_deblur_n_16b = 1'b0;
-reg ndo_deblur_r = 1'b0;
-reg n16bit_mode_r = 1'b1;
+wire posedge_nCSYNC = !vdata_r_0[3*color_width] & D_i[0];
+
+reg [1:0] data_cnt = 2'b00;
 reg nblank_rgb = 1'b1;
+reg [`VDATA_FU_SLICE] vdata_r_0 = {vdata_width{1'b0}}; // buffer for sync, red, green and blue
+reg [`VDATA_FU_SLICE] vdata_r_1 = {vdata_width{1'b0}}; // buffer for sync, red, green and blue
 
-wire negedge_nVSYNC = !vdata_r_0[3*color_width+3] &  D_i[3];
-wire posedge_nCSYNC = !vdata_r_0[3*color_width  ] &  D_i[0];
+
+always @(posedge VCLK or negedge nRST)  // data register management
+  if (!nRST)
+    data_cnt <= 2'b00;
+  else begin
+    if (!nDSYNC)
+      data_cnt <= 2'b01;  // reset data counter
+    else
+      data_cnt <= data_cnt + 1'b1;  // increment data counter
+  end
 
 
-always @(posedge VCLK)
-  if (!nDSYNC) begin
-    if (negedge_nVSYNC)
-      fetch_deblur_n_16b <= 1'b1;
-
+always @(posedge VCLK or negedge nRST)
+  if (!nRST) begin
+    nblank_rgb <= 1'b1;
+  end else if (!nDSYNC) begin
     if (ndo_deblur) begin
       nblank_rgb <= 1'b1;
     end else begin
       if(posedge_nCSYNC) // posedge nCSYNC -> reset blanking
-        nblank_rgb <= vmode;
+        nblank_rgb <= palmode;
       else
         nblank_rgb <= ~nblank_rgb;
     end
+  end
+  
+always @(posedge VCLK or negedge nRST)
+  if (!nRST)begin // data register management
+    vdata_r_0 <= {vdata_width{1'b0}};
+    vdata_r_1 <= {vdata_width{1'b0}};
   end else begin
-    if (fetch_deblur_n_16b & data_cnt == 2'b01) begin
-      fetch_deblur_n_16b <= 1'b0;
-      ndo_deblur_r <= ndo_deblur;
-      n16bit_mode_r <= n16bit_mode;
+    if (!nDSYNC) begin
+      // shift data to output registers
+      if (ndo_deblur)
+        vdata_r_1[`VDATA_SY_SLICE] <= vdata_r_0[`VDATA_SY_SLICE];
+      if (nblank_rgb)  // deblur active: pass RGB only if not blanked
+        vdata_r_1[`VDATA_CO_SLICE] <= vdata_r_0[`VDATA_CO_SLICE];
+
+      // get new sync data
+      vdata_r_0[`VDATA_SY_SLICE] <= D_i[3:0];
+    end else begin
+      // demux of RGB
+      case(data_cnt)
+        2'b01: vdata_r_0[`VDATA_RE_SLICE] <= n16bit_mode ? D_i : {D_i[6:2], 2'b00};
+        2'b10: begin
+          vdata_r_0[`VDATA_GR_SLICE] <= n16bit_mode ? D_i : {D_i[6:1], 1'b0};
+          if (!ndo_deblur)
+            vdata_r_1[`VDATA_SY_SLICE] <= vdata_r_0[`VDATA_SY_SLICE];
+        end
+        2'b11: vdata_r_0[`VDATA_BL_SLICE] <= n16bit_mode ? D_i : {D_i[6:2], 2'b00};
+      endcase
     end
   end
 
-always @(posedge VCLK) begin // data register management
-  if (!nDSYNC) begin
-    // shift data to output registers
-    if (ndo_deblur_r)
-      vdata_r_1[`VDATA_SY_SLICE] <= vdata_r_0[`VDATA_SY_SLICE];
-    if (nblank_rgb)  // deblur active: pass RGB only if not blanked
-      vdata_r_1[`VDATA_CO_SLICE] <= vdata_r_0[`VDATA_CO_SLICE];
-
-    // get new sync data
-    vdata_r_0[`VDATA_SY_SLICE] <= D_i[3:0];
-  end else begin
-    // demux of RGB
-    case(data_cnt)
-      2'b01: vdata_r_0[`VDATA_RE_SLICE] <= n16bit_mode_r ? D_i : {D_i[6:2], 2'b00};
-      2'b10: begin
-        vdata_r_0[`VDATA_GR_SLICE] <= n16bit_mode_r ? D_i : {D_i[6:1], 1'b0};
-        if (!ndo_deblur_r)
-          vdata_r_1[`VDATA_SY_SLICE] <= vdata_r_0[`VDATA_SY_SLICE];
-      end
-      2'b11: vdata_r_0[`VDATA_BL_SLICE] <= n16bit_mode_r ? D_i : {D_i[6:2], 2'b00};
-    endcase
-  end
-end
+assign vdata_sy_0_o = vdata_r_0[`VDATA_SY_SLICE];
+assign vdata_1_o = vdata_r_1;
 
 endmodule
